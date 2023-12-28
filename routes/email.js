bb5c7138ad7500f0f5b0952349f37d120664b033
satriyarifki <rifkisatriya@gmail.com>
2023-12-28 13:07:39 +0700

    fix(email) added email function for voting

--- routes/email.js
+++ routes/email.js
@@ -94,41 +94,51 @@ router.post("/send", async (req, res, next) => {
   // console.log(req.body);
 });
 
-router.post("/send/vote-email", async (req, res, next) => {
-  let transporter = nodemailer.createTransport({
-    // service: 'gmail',
-    host: "mail.aio.co.id",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: "appskjy@aio.co.id",
-      pass: "Plicakjy1234",
-    },
-    from: "appskjy@aio.co.id",
-  });
-  let participants = [];
-  req.body.participants.split(",").forEach((element) => {
-    participants.push[{ email: element }];
-  });
-  var mail = {
-    // sender address
-    from: req.body.organizer[1] + "<appskjy@aio.co.id>",
-    to: req.body.participants, // list of receivers
-    subject: "Vote Acceptance Reminder ", // Subject line
-    // html: '<b>' + req.body.message + '</b>', // html body
-    text: req.body.title + req.body.message, // plain text body
-  };
+router.post("/send/vote", async (req, res, next) => {
+  try {
+    console.log(req.body);
+    let transporter = nodemailer.createTransport({
+      // service: 'gmail',
+      host: "mail.aio.co.id",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: "appskjy@aio.co.id",
+        pass: "Plicakjy1234",
+      },
+      from: "appskjy@aio.co.id",
+    });
 
-  // send mail with defined transport object
-  let infos = await transporter.sendMail(mail, function (error, info) {
-    if (error) {
-      res.send(error);
-    } else {
-      // console.log(info);
-      res.send(info);
-    }
-  });
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(infos));
-})
+    const text =
+       
+      "Halo <br><br> Semoga pesan ini menemui Anda dalam keadaan baik. Kami sangat senang untuk mengundang Anda untuk berpartisipasi dalam proses pemungutan suara untuk waktu pelaksanaan acara <b>" +
+      req.body.title +
+      "</b>. <br> <br> Untuk memberikan suara, silakan ikuti petunjuk di bawah ini:<br> \n 1. Kunjungi [http://192.168.9.47/aio-meet/] untuk mengakses platform pemungutan suara online.<br> \n 2. Lakukan Login dengan menggunakan NIK dan Password <br> \n 3. Buka Notifikasi dan pilih Voting dengan judul : <b>" +
+      req.body.title +
+      "</b> <br> \n 4. Pilih waktu dengan centang checkbox lalu lakukan save <br> <br> Atas Perhatiannya, Terima Kasih";
+    var mail = {
+      // sender address
+      from: req.body.organizer[1] + "<appskjy@aio.co.id>",
+      to: req.body.participants, // list of receivers
+      subject: "Invitation to Submit Your Vote for " + req.body.title, // Subject line
+      html: '<font size="+1">' + text + "</font>",
+      text: text,
+    };
+
+    // send mail with defined transport object
+    let infos = transporter.sendMail(mail, function (error, info) {
+      if (error) {
+        res.send(error);
+      } else {
+        // console.log(info);
+        res.send(info);
+      }
+    });
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(infos));
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error.message);
+  }
+});
 
 module.exports = router;
